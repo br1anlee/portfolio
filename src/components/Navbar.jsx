f19b266c325ea8b34bdabf54866662b5aa831f7d
br1anlee <joonmolee3@gmail.com>
2022-04-09 22:05:01 -0700

    logo brings user back to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,18 +8,14 @@ export default function Navbar() {
     const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+      <div className='cursor-pointer'>
+        <Link to="home" smooth={true} duration={500}>
+          <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        </Link>
       </div>
 
       {/* Menu */}
         <ul className="hidden md:flex">
-          <li>
-            <Link to="home" smooth={true} duration={500}>
-              Home
-            </Link>
-          </li>
-          
           <li>
             <Link to="about" smooth={true} duration={500}>
               About
